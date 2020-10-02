e95e615251e151962c5d55cd6f36f4a255bb89fe
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-10-02 15:01:01 +0800

    Version 2.1.0 | Use config ctrl over all and split from functional parameters

--- config.h
+++ config.h
@@ -5,6 +5,8 @@
 #ifndef PROJECT_CONFIG_H
 #define PROJECT_CONFIG_H
 
+#include <unordered_map>
+
 typedef unsigned int uint;
 
 namespace config
@@ -23,6 +25,38 @@ namespace config
         MINU, MULT, DIV, LSS, LEQ, GRE, GEQ, EQL, NEQ, COLON,
         ASSIGN, SEMICN, COMMA, LPARENT, RPARENT, LBRACK, RBRACK, LBRACE, RBRACE,
         EMPTY
+    };
+
+    const std::unordered_map<TokenCode, std::string> TOKEN2STR = {
+            {IDENFR, "IDENFR"}, {INTCON, "INTCON"}, {CHARCON, "CHARCON"},
+            {STRCON, "STRCON"}, {CONSTTK, "CONSTTK"}, {INTTK, "INTTK"},
+            {CHARTK, "CHARTK"}, {VOIDTK, "VOIDTK"}, {MAINTK, "MAINTK"},
+            {IFTK, "IFTK"}, {ELSETK, "ELSETK"}, {SWITCHTK, "SWITCHTK"},
+            {CASETK, "CASETK"}, {DEFAULTTK, "DEFAULTTK"}, {WHILETK, "WHILETK"},
+            {FORTK, "FORTK"}, {SCANFTK, "SCANFTK"}, {PRINTFTK, "PRINTFTK"},
+            {RETURNTK, "RETURNTK"}, {PLUS, "PLUS"}, {MINU, "MINU"},
+            {MULT, "MULT"}, {DIV, "DIV"}, {LSS, "LSS"},
+            {LEQ, "LEQ"}, {GRE, "GRE"}, {GEQ, "GEQ"},
+            {EQL, "EQL"}, {NEQ, "NEQ"}, {COLON, "COLON"},
+            {ASSIGN, "ASSIGN"}, {SEMICN, "SEMICN"}, {COMMA, "COMMA"},
+            {LPARENT, "LPARENT"}, {RPARENT, "RPARENT"}, {LBRACK, "LBRACK"},
+            {RBRACK, "RBRACK"}, {LBRACE, "LBRACE"}, {RBRACE, "RBRACE"},
+            {EMPTY, "EMPTY"}
+    };
+
+    const std::unordered_map<std::string, TokenCode> STR2TOKEN = {
+            {"const", CONSTTK}, {"int", INTTK}, {"char", CHARTK},
+            {"void", VOIDTK}, {"main", MAINTK}, {"if", IFTK},
+            {"else", ELSETK}, {"switch", SWITCHTK}, {"case", CASETK},
+            {"default", DEFAULTTK}, {"while", WHILETK}, {"for", FORTK},
+            {"scanf", SCANFTK}, {"printf", PRINTFTK}, {"return", RETURNTK},
+            {"+", PLUS}, {"-", MINU}, {"*", MULT},
+            {"/", DIV}, {"<", LSS}, {"<=", LEQ},
+            {">", GRE}, {">=", GEQ}, {"==", EQL},
+            {"!=", NEQ}, {":", COLON}, {"=", ASSIGN},
+            {";", SEMICN}, {",", COMMA}, {"(", LPARENT},
+            {")", RPARENT}, {"[", LBRACK}, {"]", RBRACK},
+            {"{", LBRACE}, {"}", RBRACE}
     };
 
     const std::string RESERVED[] = {"const", "int", "char", "void", "main", "if", "else", "switch", "case", "default",
