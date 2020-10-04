4fc65a84ff58614a9ce0f8587a44b5cf760c287a
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-10-04 23:29:38 +0800

    Version 2.5.2 | Implement Main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 
 #include "core_lexic_Lexic.h"
+#include "core_syntactic_Syntactic.h"
 
 int main()
 {
     PeekableQueue queue;
+    symbol::SymbolManager symbolManager;
+
     lexic::Lexic instance_Lexic("testfile.txt", "output.txt", &queue);
+    syntactic::Syntactic instance_Syntactic("output.txt", &queue, &symbolManager);
 
     instance_Lexic.run();
+    instance_Syntactic.parseProgram();
 
     return 0;
 }
