d2f8aacece932cf3e114b0785383a610043002f4
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-12-21 21:58:52 +0800

    Version 6.9.3 | Unuse $v1

--- config.h
+++ config.h
@@ -213,7 +213,6 @@ namespace config
     const std::vector<std::string> InitLocalRegs = {
             "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "$t9",
             "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
-            "$v1",
     };
     const std::initializer_list<std::string> ParamRegs = {"$a0", "$a1", "$a2", "$a3"};
     const std::string zeroReg = "$zero";
